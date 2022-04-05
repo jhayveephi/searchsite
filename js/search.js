79a3ce44555a95f06c118c9e5533acb432ecb23a
jhayveephi <jhayveephi@gmail.com>
2022-04-05 13:45:55 -0700

    first commit

---
+++ js/search.js
@@ -0,0 +1,51 @@
+
+ 
+//Search a specific site rather than the entire Web.
+ 
+function search() {
+    var site = 'wikipedia.org'; 
+
+    var lookfor = document.getElementById("txtlookfor").value;
+
+    if (lookfor.length > 0) 
+    {
+        var query="http://www.google.com/search?q=" 
+        var ddchoice = document.getElementById('dropdown').value; 
+ 
+if (ddchoice == 1) {
+ 
+var query = 
+ 
+'http://www.bing.com/search?q=' + 
+ 
+encodeURIComponent(lookfor) +
+ 
+' site:' + 
+ 
+site; 
+ 
+} else { 
+ 
+var query = 
+ 
+'http://www.google.com/search?q=' + 
+ 
+encodeURIComponent(lookfor) +
+ 
+' site:' + 
+ 
+site; 
+}
+        
+        location.href = query;
+
+    }
+    else
+    {
+        alert('Please type the word or words for which you want to search.');
+
+    }
+
+
+
+}
